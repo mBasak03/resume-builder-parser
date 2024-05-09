daa7e12910ecdaff8788d60681ae851d3e754790
basakMayukh <mayukhbasak2003@gmail.com>
2024-05-09 15:59:32 +0530

    Hero section and Navbar created

---
+++ app/components/FlexboxSpacer.tsx
@@ -0,0 +1,13 @@
+export const FlexboxSpacer=({
+    maxWidth,
+    minWidth=0,
+    className=""
+}:{
+    maxWidth: number;
+    minWidth?:number;
+    className?:string
+})=>{
+    return (
+        <div className={`invisible shrink-[1000] grow ${className}`} style={{maxWidth: `${maxWidth}px`, minWidth: `${minWidth}px`}} />
+    )
+}
